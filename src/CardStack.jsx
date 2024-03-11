0bbf49100db49f0cc7a93e5b4546a06daae64f9f
Jared <jared_glenn@yahoo.com>
2024-03-11 13:49:07 -0600

    first commit

---
+++ src/CardStack.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card.jsx";
+import axios from "axios";
+
+const CardStack = () => {
+    const [ deckId, setDeckId ] useState(null);
+    const [ cardList, setCardList ] = useState(null);
+
+    useEffect(function fetchShuffleOnline() {
+        async function fetchShuffle() {
+            const deckRes = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            
+            setDeckId(deckId => deckRes.data["deck_id"]);
+        }
+    }
+
+    return (
+        <>
+            <button onClick={ addCard }>Draw A Card!</button>
+            <button onClick={ reshuffle }>Reshuffle!</button>
+            <div classFor=cardArea>
+
+            </div>
+        </>
+    )
+};
+
+export default CardStack;
