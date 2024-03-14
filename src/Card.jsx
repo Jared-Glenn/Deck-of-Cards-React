6df4cba3d508f27c325af1130fa9e58325d5632c
Jared <jared_glenn@yahoo.com>
2024-03-13 22:45:07 -0600

    add Card component and requisite images

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const Card = ({ value, suit, image }) => {
+
+    return (
+        <>
+            <img src={image}></img>
+        </>
+    )
+};
+
+export default Card;
--- src/CardStack.jsx
+++ src/CardStack.jsx
@@ -3,23 +3,42 @@ import Card from "./Card.jsx";
 import axios from "axios";
 
 const CardStack = () => {
-    const [ deckId, setDeckId ] useState(null);
-    const [ cardList, setCardList ] = useState(null);
+    const [ deckId, setDeckId ] = useState(null);
+    const [numDecks, setNumDecks] = useState(0);
+    const [ cardList, setCardList ] = useState([]);
 
     useEffect(function fetchShuffleOnline() {
         async function fetchShuffle() {
-            const deckRes = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            const deckRes = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/");
             
             setDeckId(deckId => deckRes.data["deck_id"]);
+            setCardList(list => [])
         }
-    }
+        fetchShuffle();
+    }, [numDecks]);
+
+    async function reshuffle() {
+        setNumDecks(n => n+1);
+    };
+
+    async function addCard() {
+        const cardRes = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`);
+        const newCard = {
+            "value": cardRes.data.cards[0].value,
+            "suit": cardRes.data.cards[0].suit,
+            "image": cardRes.data.cards[0].image
+        };
+        setCardList(list => [ ...list, newCard ])
+    };
 
     return (
         <>
             <button onClick={ addCard }>Draw A Card!</button>
             <button onClick={ reshuffle }>Reshuffle!</button>
             <div className='cardArea'>
-
+                {cardList.map(card => (
+                    <Card value={card.value} suit={card.suit} image={card.image} />
+                ))}
             </div>
         </>
     )
